896413e57d409fe877857a5754b56d6f39db0dc2
Daniel Kim <daniel.kim.tor15@gmail.com>
2022-07-30 17:36:32 -0400

    added .env and .gitignore

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require('mongoose')
 const encrypt = require('mongoose-encryption'); 
+
 
 const app = express();
 
@@ -19,9 +21,7 @@ const userSchema = mongoose.Schema({
     password: String
 });
 
-const secret = 'key';
-
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = mongoose.model("User", userSchema);
 
